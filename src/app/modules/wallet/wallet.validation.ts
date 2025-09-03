141a0cdf18f357f6a05286749c1e60d82691101e
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-09-03 17:16:47 +0600

    wallet validation zod schema

---
+++ src/app/modules/wallet/wallet.validation.ts
@@ -0,0 +1,32 @@
+import z from "zod";
+import { WalletStatus } from "./wallet.interface";
+
+export const addOrWithdrewMoneyZodSchema = z.object({
+  amount: z
+    .number()
+    .min(0)
+    .int()
+    .positive({ message: "Balance must be positive Number" }),
+});
+
+export const sendMoneyZodSchema = z.object({
+  receiverId: z.string(),
+  amount: z
+    .number()
+    .min(0)
+    .int()
+    .positive({ message: "Balance must be positive Number" }),
+});
+
+export const createWalletZodSchema = z.object({
+  user: z.string(),
+  balance: z
+    .number()
+    .min(0)
+    .int()
+    .positive({ message: "Balance must be positive Number" }),
+});
+
+export const updateWalletZodSchema = z.object({
+  status: z.enum(Object.values(WalletStatus) as [string]),
+});
